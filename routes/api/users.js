2c93d3cbc76de093d425858b055de6c223ae53b2
AzureXX <ismat.valiyev@gmail.com>
2018-12-14 18:43:26 +0400

    register and login api implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,73 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
-router.get("/", (req, res) => {
-  const newUser = new User({
-    email: "example@example.com",
-    password: "example"
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
+
+router.post("/create", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "Account with such email already exist" });
+    } else {
+      const newUser = new User({
+        email: email
+      });
+      if (!password) return res.json({ error: "No password entered" });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => {
+              return res.json(user);
+            })
+
+            .catch(err => console.log(err));
+        });
+      });
+    }
   });
-  newUser
-    .save()
-    .then(user => res.json(user))
-    .catch(err => console.log(err));
 });
 
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.json({ error: "User not found" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //User Matched
+        const payload = {
+          id: user._id,
+          name: user.name,
+          avatar: user.avatar,
+          role: user.role
+        };
+        //Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({ success: true, token: "Bearer " + token });
+          }
+        );
+      } else {
+        return res.status(400).json({error: "Password is incorrect"});
+      }
+    });
+  });
+});
 module.exports = router;
