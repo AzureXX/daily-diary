22bc5e155d9b69d0abaea25763b3865f626f2f87
AzureXX <ismat.valiyev@gmail.com>
2018-12-19 14:21:56 +0400

    get routes for posts by id and data

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,6 +15,34 @@ router.get(
   }
 );
 
+router.get(
+  "/date/:date",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.find({ user: req.user._id, date: req.params.date })
+      .sort({ createdAt: -1 })
+      .then(posts => res.json(posts))
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+router.get(
+  "/id/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (post.user.toString() == req.user._id.toString())
+          return res.json(post);
+        else {
+          //Throw empty object for unknown reason
+          throw new Error("No access")
+        }  
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 router.post(
   "/add",
   passport.authenticate("jwt", { session: false }),
@@ -31,4 +59,5 @@ router.post(
       .catch(err => res.status(404).json(err));
   }
 );
+
 module.exports = router;
