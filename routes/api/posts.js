2250736a7b7071be03a0f3103d22c2cfcb48ae09
AzureXX <ismat.valiyev@gmail.com>
2018-12-19 16:30:50 +0400

    update and delete of posts by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -36,8 +36,8 @@ router.get(
           return res.json(post);
         else {
           //Throw empty object for unknown reason
-          throw new Error("No access")
-        }  
+          throw new Error("No access");
+        }
       })
       .catch(err => res.status(404).json(err));
   }
@@ -60,4 +60,42 @@ router.post(
   }
 );
 
+router.put(
+  "/edit/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        post.type = req.body.type;
+        post.details = req.body.details || {};
+        post.updatedAt = Date.now();
+        post
+          .save()
+          .then(post => res.json(post))
+          .catch(err => res.status(404).json(err));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+router.delete(
+  "/remove/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if(!post) throw new Error("No such post exist")
+        if (post.user.toString() !== req.user.id) {
+          return res
+            .status(401)
+            .json({ notauthorized: "User is not authorized" });
+        }
+        post
+          .remove()
+          .then(() => res.json({ success: true }))
+          .catch(err => res.status(404).json(err));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 module.exports = router;
